0b2d437ceb93dba57ecc589ec53f40053b982b93
jake <jbearden364@gmail.com>
2023-01-25 10:31:24 -0800

    .

---
+++ find_max.cpp
@@ -0,0 +1,17 @@
+// find max
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a , b , c , max;
+
+    cout << "Enter number" << endl;
+    cin >> a;
+    cout << "Enter number" << endl;
+    cin >> b;
+    cout << "Enter number" << endl;
+    cin >> c;
+    
+}
